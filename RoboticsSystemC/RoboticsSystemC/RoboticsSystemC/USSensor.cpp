5e8dcde96fcb15f35d3c971e401c3c0174990099
tac51 <tac51@pitt.edu>
2013-02-18 12:00:37 -0500

    Update RoboticsSystemC/RoboticsSystemC/RoboticsSystemC/USSensor.cpp

--- RoboticsSystemC/RoboticsSystemC/RoboticsSystemC/USSensor.cpp
+++ RoboticsSystemC/RoboticsSystemC/RoboticsSystemC/USSensor.cpp
@@ -3,9 +3,11 @@
 USSensor::USSensor(Orientation::Direction dir)
 {
 	orientation = dir;
+	pin = 0;
 }
 
-int USSensor::get_distance()
+int USSensor::get_distance(int pin)
 {
-	return 0;
+	pinMode(pin,INPUT);
+	return(analogRead(pin));
 }
