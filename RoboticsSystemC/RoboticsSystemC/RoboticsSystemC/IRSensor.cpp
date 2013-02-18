f2fe06b0b661cc14a08e1a98a9b619b0ac292d1c
tac51 <tac51@pitt.edu>
2013-02-18 11:57:29 -0500

    Update RoboticsSystemC/RoboticsSystemC/RoboticsSystemC/IRSensor.cpp

--- RoboticsSystemC/RoboticsSystemC/RoboticsSystemC/IRSensor.cpp
+++ RoboticsSystemC/RoboticsSystemC/RoboticsSystemC/IRSensor.cpp
@@ -4,10 +4,12 @@ IRSensor::IRSensor(Orientation::Direction dir)
 {
 	//default void contstructor
 	orientation = dir;
+	pin = 0;
 }
 
-int IRSensor::get_distance()
+int IRSensor::get_distance(int pin)
 {
 	//gets the distance for the sensor
-	return 0;
+	pinMode(pin,INPUT);
+	return(analogRead(pin));
 }
